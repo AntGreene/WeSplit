5fb8227367626bb52e6b5c2548033cbfd230c1f9
Anthony Greene <s3770653@students.fscj.edu>
2025-09-14 16:17:36 -0500

    Hiding the keyboard

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [10 , 15, 20 , 25 , 0]
     
@@ -30,6 +31,7 @@ struct ContentView: View {
         Form {
             Section{
                 TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .focused($amountIsFocused)
                 
                 Picker("Number of people", selection: $numberOfPeople){
                     ForEach(2..<100){
@@ -51,6 +53,13 @@ struct ContentView: View {
             }
         }
         .navigationTitle("WeSplit")
+        .toolbar {
+            if amountIsFocused {
+                Button("Done"){
+                    amountIsFocused = false
+                }
+            }
+        }
     }
 }
 
