0322fd90986ef7ca14486cd943aeb326cd6390bc
Anthony Greene <s3770653@students.fscj.edu>
2025-09-14 14:56:22 -0500

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by user279044 on 9/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by user279044 on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
